55361676a17748d33b9cc48b960469510707bb95
Brendan Murphy <brendan.murphy@ostusa.com>
2024-05-10 14:44:52 -0400

    set up neon connection

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import CentralDisplay from '@/components/CentralDisplay';
+import  Page  from '../lib/db';
 
 export default function Home() {
+  const test = Page()
   return (
     <div className='mx-16 '>
       <CentralDisplay />
