7ff4fd5ce86ea13e7fb249a0fdf1f883269eacbe
Brendan Murphy <brendan.murphy@ostusa.com>
2024-07-07 18:56:12 -0400

    Does not create duplicate author

--- actions/bookActions.ts
+++ actions/bookActions.ts
@@ -5,6 +5,8 @@ import { revalidatePath } from "next/cache";
 import db from "../lib/db";
 import { book, author } from "../lib/schema";
 import { Pool } from '@neondatabase/serverless';
+import { Book } from '@/types/book';
+import { date } from 'drizzle-orm/mysql-core';
 
 
 export const getData = async (status?: string) => {
@@ -17,30 +19,35 @@ export const getData = async (status?: string) => {
 };
 
 export const addBook = async (title: string, authorName: string, authorImg: string, status: number, image: string) => {
-    console.log("Here")
 
     // create a `Client` inside the request handler
     const pool = new Pool({ connectionString: process.env.DATABASE_URL });
     pool.on('error', err => console.error(err));  // deal with e.g. re-connect
     const client = await pool.connect();
 
+    const authorData = await db.select().from(author).where(eq(author.name, authorName));
+    
+    //author does not exist so we need to start transaction to add them as well 
+    if (authorData.length == 0) {
+        try {
+            await client.query('BEGIN');
+            console.log("About to insert author")
+            const { rows: [{ id: authorId }] } = await client.query('INSERT INTO author (name, image) VALUES ($1, $2) RETURNING id', [authorName, authorImg]);
+            console.log("About to insert book")
+            console.log(authorId)
+            await client.query('INSERT INTO book (title, author_id, image, status) VALUES ($1,$2, $3, $4)', [title, authorId, image, status])
+            await client.query('COMMIT');
+        } catch (err) {
+            await client.query('ROLLBACK');
+            throw err;
 
-    try {
-        await client.query('BEGIN');
-        console.log("About to insert author")
-        const { rows: [{ id: authorId }] } = await client.query('INSERT INTO author (name, image) VALUES ($1, $2) RETURNING id', [authorName, authorImg]);
-        console.log("About to insert book")
-        console.log(authorId)
-        await client.query('INSERT INTO book (title, author_id, image, status) VALUES ($1,$2, $3, $4)', [title, authorId, image, status])
-        await client.query('COMMIT');
-    }catch (err) {
-        await client.query('ROLLBACK');
-        throw err;
-      
-      } finally {
-        client.release();
-      }
+        } finally {
+            client.release();
+            return;
+        }
+    }
 
+    await client.query('INSERT INTO book (title, author_id, image, status) VALUES ($1,$2, $3, $4)', [title, authorData[0].id, image, status])
     revalidatePath("/");
 };
 
