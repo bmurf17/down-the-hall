764cbf7d4932eb93d951b2201dc604d25b00cb2e
bmurf17 <bmurf17@gmail.com>
2024-11-06 16:05:15 -0500

    one more try on build error

--- app/find/page.tsx
+++ app/find/page.tsx
@@ -30,7 +30,7 @@ async function BookResults({ searchTitle }: { searchTitle: string }) {
           <AlertCircle className="h-4 w-4" />
           <AlertTitle>No books found</AlertTitle>
           <AlertDescription>
-            We couldn't find any books matching {searchTitle};. Try adjusting
+            We couldn't find any books matching {searchTitle}. Try adjusting
             your search terms.
           </AlertDescription>
         </Alert>
