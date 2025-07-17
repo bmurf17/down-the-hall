ede7f27b5b285e727ad6825c72c4f281b254bd5d
bmurf17 <bmurf17@gmail.com>
2025-07-17 14:06:48 -0400

    remove commented code

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -17,17 +17,6 @@ export default async function UserPage() {
 
     const response = await getUsers();
 
-    // if (!response.ok) {
-    //   const errorText = await response.text();
-    //   return (
-    //     <div className="min-h-screen flex items-center justify-center">
-    //       <p className="text-lg text-red-500">
-    //         Failed to load users: {response.status} {errorText}
-    //       </p>
-    //     </div>
-    //   );
-    // }
-
     const userGridResponse: userGridResponse[] = response;
 
     return (
